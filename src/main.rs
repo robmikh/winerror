7db3ab6ea90560f19764db82831c8212905200ff
Robert Mikhayelyan <rob.mikh@outlook.com>
2020-02-13 22:20:33 -0800

    start some tests

--- src/main.rs
+++ src/main.rs
@@ -45,7 +45,7 @@ fn parse_code(input: &str) -> i32 {
         // inputs like 0x80070005 would fail.
         i64::from_str_radix(input.trim_start_matches("0x"), 16).unwrap() as i32
     } else {
-        // We do this to get around overflow checks. Otherwise,
+        // We do this to get around underflow and overflow checks. Otherwise,
         // inputs like -2147942405 would fail.
         let input = input.replace("-", "");
         input.parse::<i64>().unwrap() as i32
@@ -69,29 +69,44 @@ fn main() {
 
 #[cfg(test)]
 mod tests {
-    // TODO: Write a real test
     #[test]
     #[allow(overflowing_literals)]
-    fn scratch() {
+    fn some_codes_lookup_test() {
         let codes = vec![
-            0x00030200,
-            197120,
-            0,
-            88,
-            0x80070005,
-            -2147942405,
-            2147942405,
+            (0x00030200, true),
+            (197120, true),
+            (0, true),
+            (0x80070000, false),
+            (0x80070005, true),
+            (-2147942405, false),
+            (2147942405, true),
         ];
-
-        println!("Running test...");
-        for code in codes {
+        
+        let mut temp = 0;
+        for (code, will_succeed) in codes {
             if let Some(info) = crate::get_error_info_from_code(code) {
-                println!("Code: {:#010x}", info.code);
-                println!("Name: {}", info.name);
-                println!("Description: {}", info.description);
+                assert!(will_succeed);
+                assert_eq!(code, info.code);
             } else {
-                println!("Not found!");
+                if will_succeed {
+                    panic!("Unexpected lookup failure! {} ({:#010x}) was expected to succeed! {}", code, code, temp);
+                }
             }
+            temp = temp + 1;
         }
+    }
+
+    #[test]
+    #[allow(overflowing_literals)]
+    fn negative_input_test() {
+        let code = crate::parse_code("-2147942405");
+        assert_eq!(0x80070005, code);
+    }
+
+    #[test]
+    #[allow(overflowing_literals)]
+    fn hex_input_test() {
+        let code = crate::parse_code("0x80070005");
+        assert_eq!(0x80070005, code);
     }
 }
